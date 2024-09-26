a4b08685babcef6c04863e62e1fea625fcd209a3
Artem Petrov <artem.petrov@underarmour.com>
2024-09-26 16:00:23 +0300

    screnshotListener changed

--- src/test/java/com/solvd/task1/ScreenshotListener.java
+++ src/test/java/com/solvd/task1/ScreenshotListener.java
@@ -19,21 +19,9 @@ import com.zebrunner.agent.core.registrar.Screenshot;
 public class ScreenshotListener extends TestListenerAdapter {
     @Override
     public void onTestFailure(ITestResult result) {
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-        String methodName = result.getName();
         if (!result.isSuccess()) {
-            File scrFile = ((TakesScreenshot) WebDriverPool.get()).getScreenshotAs(OutputType.FILE);
             byte[] screenshotBytes = ((TakesScreenshot) WebDriverPool.get()).getScreenshotAs(OutputType.BYTES);
             Screenshot.upload(screenshotBytes, Calendar.getInstance().getTimeInMillis());
-            try {
-                String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
-                File destFile = new File((String) reportDirectory + "/failure_screenshots/" + methodName + "_" + formater.format(calendar.getTime()) + ".png");
-                FileUtils.copyFile(scrFile, destFile);
-                Reporter.log("<a href='" + destFile.getAbsolutePath() + "'> <img src='" + destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
