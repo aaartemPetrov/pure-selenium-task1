f98f9b15aa5fd165d5a3495174a500c34e1f12a1
Artem Petrov <artem.petrov@underarmour.com>
2024-09-26 13:48:58 +0300

    setUp change

--- src/test/java/com/solvd/task1/AbstractTest.java
+++ src/test/java/com/solvd/task1/AbstractTest.java
@@ -6,10 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public abstract class AbstractTest {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(AbstractTest.class);
+
+    @BeforeSuite
+    public void preSetup() {
+
+    }
 
     @BeforeMethod
     public void setup() {
